5f6944babb971cd75271d3d7b8feac4877b4c603
Дмитрий Ковыляев <354981@mail.ru>
2021-05-18 23:14:32 +0500

    sdfsdf

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -4,6 +4,6 @@ const waApi = WolframAlphaAPI('2J9EYY-4LE93R34PJ');
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({message: waApi.getFull('sin x')})
+        body: waApi.getFull({ input:'4*5', output:'json'})
     };
 }
