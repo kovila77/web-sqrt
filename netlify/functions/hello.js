89fcf5317dcb50b3a90edb468ba12b9796b58339
Дмитрий Ковыляев <354981@mail.ru>
2021-05-18 23:27:02 +0500

    rgdg

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,8 +2,9 @@ const WolframAlphaAPI = require('wolfram-alpha-api');
 const waApi = WolframAlphaAPI('2J9EYY-4LE93R34PJ');
 
 exports.handler = async function(event, context) {
+    const output = waApi.getFull({ input:'4*5', output:'json'})
     return {
         statusCode: 200,
-        body: waApi.getFull({ input:'4*5', output:'json'})
+        body: JSON.stringify({message: output})
     };
 }
