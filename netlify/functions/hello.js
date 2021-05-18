c4ab34017422c88438a6460a6cc52d763980c1f4
Дмитрий Ковыляев <354981@mail.ru>
2021-05-18 22:41:19 +0500

    add wolfram-alpha-api

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,9 @@
+const WolframAlphaAPI = require('wolfram-alpha-api');
+const waApi = WolframAlphaAPI('DEMO-APPID');
+
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({message: waApi.getFull('sin x')})
     };
 }
